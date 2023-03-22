5ba873e1ef08b39cc9ea83e2219fae602920d803
Tomoshige Jimu <keririnkery.kery@gmail.com>
2023-03-22 12:15:20 +0900

    Create users_controller
    rails g scaffold_controller user email:string crypted_password:string salt:string 実行
    ルーティング修正
    usersコントローラ修正
    new.html.erb修正
    不要ファイル削除(newとcreate以外)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+
+  validates :email, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
